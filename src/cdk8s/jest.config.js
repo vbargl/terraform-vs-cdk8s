1b73125868b41e0a8410b61c28302331f2a01315
Vojtěch Bargl <bargl.vojtech@gmail.com>
2025-02-13 22:03:00 +0100

    02-init/cdk8s

---
+++ src/cdk8s/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ["<rootDir>"],
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "json",
+    "node",
+  ],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
